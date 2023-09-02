85647bb3dd90254fe21aaf7df430cdc62d6053f7
Gerry <gerardpatrickgibbons@gmail.com>
2023-09-01 20:39:17 -0400

    Initialized angular front-end

---
+++ AngularRuneDataApp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'AngularRuneDataApp';
+  subtitle = 'this is supposed to be a subtitle, I don\'t know.'
+}
